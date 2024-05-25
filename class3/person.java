e18e15abb83fed29141bfe0a1e8726a32a8fc1c6
juto-shogan <somtochukwu.mbonu@binghamuni.edu.ng>
2024-05-25 18:05:57 +0100

    inheritance

--- class3/person.java
+++ class3/person.java
@@ -14,7 +14,7 @@ public class person{
         private String password;
       
         public Student(String name, String schoolName, String matricNumber, String password) {
-          super(name, schoolName);
+          //super(name, schoolName);
           this.matricNumber = matricNumber;
           this.password = password;
         }
